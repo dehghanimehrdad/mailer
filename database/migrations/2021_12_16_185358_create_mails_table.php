2b74cb6d1bd5b6bdf87152be4c47ab985e4be44a
Mehrdad Dehghani <mehrdad.de96@gmail.com>
2021-12-19 04:15:43 +0330

    Created Mail Model and migration

---
+++ database/migrations/2021_12_16_185358_create_mails_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mails', function (Blueprint $table) {
+            $table->id();
+            $table->string('to',500);
+            $table->string('subject',500);
+            $table->text('body');
+            $table->dateTime('sent_at')->nullable()->default(null);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mails');
+    }
+}
