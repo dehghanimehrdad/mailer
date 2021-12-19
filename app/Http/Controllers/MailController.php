36062d340c651adb0df72cf4da23df5964395d5e
Mehrdad Dehghani <mehrdad.de96@gmail.com>
2021-12-19 19:21:41 +0330

    Added MailController and API Route for processing

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Jobs\SendMail;
+use App\Models\Mail;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+
+class MailController extends Controller
+{
+    public function sendMail(Request $request){
+        // iterate for each mail request
+        foreach ($request->json()->all() as $mailRequest){
+            // validate request for required fields
+            Validator::make($mailRequest, [
+                'to' => 'required|max:500',
+                'subject' => 'required|max:500',
+                'body' => 'required',
+            ])->validate();
+
+            // validate attachments if present
+            if(isset($mailRequest->attachments)){
+                foreach ($mailRequest->attachments as $attachment){
+                    Validator::make($attachment, [
+                        'name' => 'required|max:500',
+                        'base64' => 'required',
+                    ])->validate();
+                }
+            }
+
+            //create mail model and store it
+            $mail = new Mail();
+            $mail->to = $mailRequest['to'];
+            $mail->subject = $mailRequest['subject'];
+            $mail->body = $mailRequest['body'];
+            $mail->save();
+
+            //set attachments on the mail model
+            foreach ($mailRequest['attachments'] as $attachment){
+                $mail->addMediaFromBase64($attachment['base64'])->usingFileName($attachment['name'])
+                    ->toMediaCollection();
+            }
+
+            //dispatch job for later processing
+            SendMail::dispatch($mail);
+
+            //send response
+            return response()->json(['success' => true]);
+        }
+    }
+}
